ac59d7fdd0d6bcbd565033faff94e8e28d93bcd7
AzeemWorsdorfer <mohazeem327@gmail.com>
2025-11-30 15:08:43 +0100

    Finalized SQLAlchemy models and initialized database creation"

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
-from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import Flask
+from models import db, init_db
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+# The database file will be created inside the 'data' folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
+    os.path.join(BASE_DIR, "data", "movie_database.db")
+# Recommended to disable modification tracking
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+init_db(app)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        print("Database tables created successfully.")
+
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def init_db(app):
+    """Initializes the SQLAlchemy object with the Flask application instance."""
+    db.init_app(app)
+
+
+class User(db.Model):
+    """Represents a user in the application."""
+    __tablename__ = 'user'  
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+    # Relationship to Movie: user.movies will return a list of Movie objects
+    # 'backref' creates a .user attribute on the Movie object to access the parent
+    movies = db.relationship('Movie', backref='user', lazy='dynamic')
+
+    def __repr__(self):
+        return f"User: ({self.name}) Id: ({self.id})"
+
+
+class Movie(db.Model):
+    """Represents a movie owned by a user."""
+    __tablename__ = 'movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100))
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String)
+
+    # Foreign Key linking the movie back to the user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Movie: ({self.name}) added by User ID: ({self.user_id})"
